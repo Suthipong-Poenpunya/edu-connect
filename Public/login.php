32ba5501377efd36c8a648f7d84f9808dc4bb32c
Suthipong-Poenpunya <6742422627@student.chula.ac.th>
2025-07-06 18:26:46 +0000

    Test webpage login

--- Public/login.php
+++ Public/login.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Test Webpage</title>
+</head>
+<body>
+  <h1>Test Webpage</h1>
+  <p>This is a test login page.</p>
+</body>
+</html>
